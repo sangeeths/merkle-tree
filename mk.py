b4a94c4368178e88bfb9e6d8f0afb21591a99f45
Sangeeth Saravanaraj <sangeeth.s@gmail.com>
2012-11-30 01:39:07 +0530

    added more functionality

--- mk.py
+++ mk.py
@@ -4,15 +4,17 @@ import fnmatch
 import os
 import md5
 import sys
+import hashlib
 
-class MarkelTree:
+class MarkleTree:
     def __init__(self, root):
         self._root      = root
         self._files     = []
         self._top_hash  = 0
         self._dirs      = []
+        self._mk        = {}
 
-    def GetDirs(self, directory):
+    def GetAllDirs(self, directory):
         for root, dirnames, filenames in os.walk(directory):
             for dirname in dirnames:
                 self._dirs.append(os.path.join(root, dirname))
@@ -20,55 +22,118 @@ class MarkelTree:
         self._dirs.append(self._root)
         self._dirs.sort()
         return
-
-    def GetFiles(self, directory):
+    
+    def GetAllFiles(self, directory):
         for root, dirnames, filenames in os.walk(directory):
             for filename in filenames:
                 self._files.append(os.path.join(root, filename))
         self._files.sort()
         return
 
-    def sumfile(self, fobj):
-        '''Returns an md5 hash for an object with read() method.'''
-        m = md5.new()
-        while True:
-            d = fobj.read(8096)
-            if not d:
-                break
-            m.update(d)
-        return m.hexdigest()
+    def GetDirs(self, directory):
+        items = os.listdir(directory)
+        dirs = []
+        for item in items:
+            if os.path.isdir(item):
+                dirs.append(os.path.join(directory, item))
+        dirs.sort()
+        return dirs
 
-    def md5sum(self, fname):
-        '''Returns an md5 hash for file fname, or stdin if fname is "-".'''
-        if fname == '-':
-            ret = self.sumfile(sys.stdin)
-        else:
-            try:
-                f = file(fname, 'rb')
+    def GetItems(self, directory):
+        items = os.listdir(directory)
+        value = []
+        for item in items:
+            value.append(os.path.join(directory, item))
+        value.sort()
+        #print value
+        return value
+
+    def DirDigest(self, directory):
+        items = self.GetItems(directory)
+        if not items:
+            self._mk[directory] = ''
+            return
+        for item in items:
+            if os.path.isdir(item):
+                self.DirDigest(item)
+                subitems = self.GetItems(item)
+                s = ''
+                for subitem in subitems:
+                    s = s + self._mk[subitem]
+                self._mk[item] = self.md5sum(s)
+
+    def GetTopHash(self):
+        items = self.GetItems(self._root)
+        if not items:
+            return ''
+        s = ''
+        for item in items:
+            s = s + self._mk[item]
+        self._mk[self._root] = self.md5sum(s)
+
+    
+
+    def A(self, directory):
+        
+        # key = md5sum
+        # value = [filename, []]
+        for f in self._files:
+            self._mk[f] = self.md5sum(f)
+        self.PrintMTree()
+
+        self.DirDigest(directory)
+        self.GetTopHash()
+        self.PrintMTree()
+        
+    
+    def md5sum(self, data):
+        m = hashlib.md5()
+        if os.path.isfile(data):
+            try:   
+                f = file(data, 'rb')
             except:
-                return 'Failed to open file'
-            ret = self.sumfile(f)
+                return 'ERROR: unable to open %s' % data 
+            while True:
+                d = f.read(8096)
+                if not d:
+                    break
+                m.update(d)
             f.close()
-        return ret
+        else:
+            m.update(data)
+        return m.hexdigest()
 
     def PrintItems(self):
         print "files: "
         for filename in self._files:
-            #print "%s - %32s" % (item, self.md5sum(item))
-            print "%s" % filename
+            print "%s - %32s" % (filename, self.md5sum(filename))
+            #print "%s" % filename
         print "Directories: "
         for dirname in self._dirs:
             print "%s" % dirname
+    
+    def PrintMTree(self):
+        print "---------------------------------------"
+        for key, value in self._mk.iteritems():
+            print "Key      : %s" % key
+            print "Value    : %s" % value
+        print "---------------------------------------"
+        return
 
     def run(self):
         self.GetDirs(self._root)
-        self.GetFiles(self._root)
+        self.GetAllFiles(self._root)
         if not self._files and not self._dirs:
             return 0
-        self.PrintItems()
-        
+        #self.PrintItems()
+        #self.GetItems(self._root)
+        #self.GetMTree(self._root)
+        #self.PrintMTree()
+        self.A(self._root)
+
+
 if __name__ == "__main__":
-        mk_a = MarkelTree('/home/sangeeth/work/github/merkle-trees/testA'); 
-        mk_a.run();
+        mt_a = MarkleTree('/home/sangeeth/work/github/merkle-tree/testA')
+        mt_a.run()
         # one = MarkelTree("/home/sangeeth/work/github/merkle-trees/testB"); 
 
