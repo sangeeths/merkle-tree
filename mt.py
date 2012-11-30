c4cf1e3a964664b5c9275f94ccd856db1928e3fd
Sangeeth Saravanaraj <sangeeth.s@gmail.com>
2012-12-01 03:35:39 +0530

    final checkin

--- mt.py
+++ mt.py
@@ -36,8 +36,6 @@ class MarkleTree:
 
     def MT(self):
         for node, hash in self._hashlist.iteritems():
-            print "nodes : ", node
-            print "hash  : ", hash
             items = self.GetItems(node)
             value = []
             value.append(node)
@@ -55,14 +53,14 @@ class MarkleTree:
         self.HashList(self._root)
         #self.PrintHashList()
         self.MT()
-        #self.PrintMT(self._tophash)
+        print "Merkle Tree for %s: " % self._root
+        self.PrintMT(self._tophash)
+        self.Line()
 
     def md5sum(self, data):
         m = hashlib.md5()
-        print "md5sum: incoming data: %s" % data
         fn = os.path.join(self._root, data)
         if os.path.isfile(fn):
-            print "md5sum: %s is a FILE" % fn
             try:   
                 f = file(fn, 'rb')
             except:
@@ -74,7 +72,6 @@ class MarkleTree:
                 m.update(d)
             f.close()
         else:
-            print "md5sum: %s is a STRING" % data
             m.update(data)
         return m.hexdigest()
 
@@ -88,15 +85,10 @@ class MarkleTree:
                 value.append(item)
                 #value.append(os.path.join(".", item))
             value.sort()
-        print "GetItems: directory = %s" % directory
-        print "GetItems: value = ", value
         return value
     
     def HashList(self, rootdir):
         self.HashListChild(rootdir)
-        print "*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@"
-        self.PrintHashList()
-        print "*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@"
         items = self.GetItems(rootdir)
         if not items:
             self._hashlist[rootdir] = ''
@@ -111,17 +103,11 @@ class MarkleTree:
         if not items:
             self._hashlist[rootdir] = ''
             return
-        #digest = ''
         for item in items:
             itemname = os.path.join(rootdir, item)
-            print "itemname = %s" % itemname
             if os.path.isdir(itemname):
-                print "%s is directory" % itemname
                 self.HashListChild(item)
                 subitems = self.GetItems(item)
-                print "subitems for %s" % item
-                print subitems
-                print self._hashlist
                 s = ''
                 for subitem in subitems:
                     s = s + self._hashlist[os.path.join(item, subitem)]
@@ -130,50 +116,32 @@ class MarkleTree:
                 else:
                     self._hashlist[itemname] = self.md5sum(s)
             else:
-                print "%s is file" % itemname
                 if rootdir == self._root:
                     self._hashlist[item] = self.md5sum(item)
                 else:
                     self._hashlist[itemname] = self.md5sum(itemname)
-   
-        #    if rootdir == self._root: 
-        #        digest = digest + self._hashlist[item]
-        #    else:
-        #        digest = digest + self._hashlist[itemname]
-        #self._hashlist[rootdir] = self.md5sum(digest)
+ 
 
-
+  
 def MTDiff(mt_a, a_tophash, mt_b, b_tophash):
     if a_tophash == b_tophash:
-        print "EQUAL TOPHASH - %s" % a_tophash
+        print "Top hash is equal for %s and %s" % (mt_a._root, mt_b._root)
     else:
         a_value = mt_a._mt[a_tophash] 
-        a_item = a_value[0] 
-        a_child = a_value[1] 
+        a_child = a_value[1]    # retrive the child list for merkle tree a
         b_value = mt_b._mt[b_tophash] 
-        b_item = b_value[0] 
-        b_child = b_value[1]
-
-        print a_item    
-        print a_child
-        print b_item    
-        print b_child
-        
+        b_child = b_value[1]    # retrive the child list for merkle tree b
 
         for itemhash, item in a_child.iteritems():
             try:
                 if b_child[itemhash] == item:
-                    print "SAME : %s" % item
+                    print "Info: SAME : %s" % item
             except:
-                print "DIFFERENT : %s" % item
+                print "Info: DIFFERENT : %s" % item
                 temp_value = mt_a._mt[itemhash]
-                if len(temp_value[1]) > 0:
-                    print "%s is a dir" % item
+                if len(temp_value[1]) > 0:      # check if this is a directory
                     diffhash = list(set(b_child.keys()) - set(a_child.keys()))
-                    
                     MTDiff(mt_a, itemhash, mt_b, diffhash[0])
-                else:
-                    print "%s is a file" % item
                 
 
 if __name__ == "__main__":
